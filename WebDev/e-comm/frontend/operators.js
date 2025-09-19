e21f50d2d12406199709732f62c0b8eba5f2f29c
notsoarsh <arshpalwork@gmail.com>
2025-09-19 21:54:21 +0530

    Learned starter for React

--- WebDev/e-comm/frontend/operators.js
+++ WebDev/e-comm/frontend/operators.js
@@ -11,4 +11,25 @@
   const userName = isLoggedIn ? "John doe" : "Guest";
   console.log(userName);
 
-  
+// optional chaining
+
+// Say we have a user
+var user1 = {
+  "academics" : {
+    "college" : "collegename",
+  }
+}
+
+//if we try to acccess user1.academics.profile.name , shows error
+// so we can use user1.academics.profile?.name; //here we check if it exists
+
+// ---------------------------------
+// (High chance in interview)
+// nullish coalescing operator - to provide default val for null or undefined
+var firstName;
+var lastName;
+var usrName;
+
+var displayName = firstName ?? lastName ?? userName ?? "guest";
+
+// Where ever we are using . operator , we should always use (?.) , to check if the value is present or not before accessing
