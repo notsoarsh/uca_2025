e44b34d7563c7caa5af941d8eb578840ddb74dd7
notsoarsh <arshpalwork@gmail.com>
2025-10-01 18:48:48 +0000

    Update CountPaths Solution

--- problem_solving/JAVA/Recursion_Assignment/CountPaths.java
+++ problem_solving/JAVA/Recursion_Assignment/CountPaths.java
@@ -38,7 +38,7 @@ public class CountPaths {
  * * */
    public int countPaths(int m, int n, int x, int y) {
      if (x == 0 && y == 0) return 0;
-     int[][] dp = new int[m + 1][n + 1];
+     int[][] dp = new int[m][n];
      for (int i = 0; i < dp.length; i++) {
        Arrays.fill(dp[i], -1);
      }       	           
@@ -52,10 +52,10 @@ public class CountPaths {
      
      //check if the answer exists
      if (dp[i][j] != -1) return dp[i][j];
-
+     //calculate the ans for left calls and top calls 	
      int top = helper(i - 1, j, dp);
      int left = helper(i, j - 1, dp);
-	
+     //store for the current index	
      dp[i][j] = top + left;
      return dp[i][j];  
    }
