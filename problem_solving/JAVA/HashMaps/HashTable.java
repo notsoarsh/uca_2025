7fac4a307a178419723991b69e2cbe43552bd325
notsoarsh <arshpalwork@gmail.com>
2025-10-21 09:48:17 +0000

    Fixed implementation for HashTable

--- problem_solving/JAVA/HashMaps/HashTable.java
+++ problem_solving/JAVA/HashMaps/HashTable.java
@@ -1,47 +1,60 @@
 public class HashTable<Key, Value> {
-  static class Node {
-    Object key;
-    Object value;
-    Node next;
 
-    Node(Object key, Object value, Node next) {
+  static class Node<K, V> {
+    K key;
+    V val;
+    Node<K , V> next;
+
+    Node(K key, V val, Node<K, V> next) {
       this.key = key;
-      this.value = value;
+      this.val = val;
       this.next = next;
     }
   }
 
+  //define the size of table
   private int M = 32;
-  private Node[] ht = new Node[M];
+  //define bucket array
+  private Node<Key, Value>[] ht;
 
+  public HashTable() {
+    this.ht = (Node<key, val>[])new Node[M];
+  }
+  
   public int hash(Key key) {
     return (key.hashCode() & 0x7fffffff) % M;
   }
 
-  public void put(Key key, Value value) {
+  public void put(Key key, Value val) {
     int i = hash(key);
-    
-    for (Node x = ht[i]; x != null; x = x.next) {
-      if (x.key.equals(key)) {
-        x.value = value;
+    for (Node<Key, Value> temp = ht[i]; temp != null; temp = temp.next) {
+      if (temp.key.equals(key)) {
+        temp.val = val;
         return;
       }
     }
-
-  //Insert a node at the start of the linked list
-  ht[i] = new Node(key, value, ht[i]);
-  
+    ht[i] = new Node<>(key, value, ht[i]);
   }
 
-  public Value get(Key key) {
-    if (key == null) return null;
+  public int get(Key key) {
     int i = hash(key);
-  
-    for (Node x = ht[i]; x != null; x = x.next) {
-      if (x.key.equals(key)) {
-        return ((Value)x.value);
+    for (Node<Key, Value> temp = ht[i]; temp != null; temp = temp.next) {
+      if (temp.key.equals(key)) {
+        return temp.val;
       }
     }
     return null;
   }
+
+  public static void main(String[] args) {
+    HashTable<String, Integer> fruits = new HashTable<>();
+    fruits.put("Apple", 10);
+    fruits.put("Orange", 12);
+    fruits.put("Apple", 5);
+    int A = fruits.get("Apple");
+    int O = fruits.get("Orange");
+    System.out.println(A + " " + O);
+  }
+  
+
 }
